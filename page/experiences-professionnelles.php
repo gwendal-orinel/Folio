c5bd95af473d80308d4e653caa567ac46f84ea1d
Gwendal Orinel <gwendal.orinel@gmail.com>
2018-10-18 14:23:09 +0200

    update-folio

--- page/experiences-professionnelles.php
+++ page/experiences-professionnelles.php
@@ -3,11 +3,12 @@
     <div class="page-header text-center " style="margin-top: -20px; font-family:'Segoe Script'; color:rgb(96, 125, 139); ">
         <h3>Expériences Professionelles</h3>
     </div>
-	<?php $ligne=1;
-	$liste_experiences->execute(); 
-	while ($donnees = $liste_experiences->fetch()){ ?>
-        <?php if($ligne==1){?><div class="col-sm-12" ><?php } ?>
-            <div class="col-sm-6">
+		<div class="col-sm-6" style="padding: 0px;">
+        <?php $ligne=1;
+        $liste_experiences->execute();
+        while ($donnees = $liste_experiences->fetch()){ ?>
+        <?php if($ligne==1){?>
+            <div class="col-sm-12">
                 <div class="panel panel-blue_grey" >
                     <div class="panel-heading"><?php echo $donnees['dates']; ?></div>
                     <div class="panel-body">
@@ -15,5 +16,21 @@
                     </div>
                 </div>
             </div>
-	<?php $ligne++; if($ligne==3){ $ligne=1; ?></div><?php } } ?>
+        <?php } $ligne++; if($ligne==3){ $ligne=1; }} ?>
+		</div>
+		<div class="col-sm-6" style="padding: 0px;">
+        <?php $ligne=1;
+        $liste_experiences->execute();
+        while ($donnees = $liste_experiences->fetch()){ ?>
+        <?php if($ligne==2){?>
+            <div class="col-sm-12">
+                <div class="panel panel-blue_grey" >
+                    <div class="panel-heading"><?php echo $donnees['dates']; ?></div>
+                    <div class="panel-body">
+                        <?php echo $donnees['description']; ?>
+                    </div>
+                </div>
+            </div>
+        <?php } $ligne++; if($ligne==3){ $ligne=1; }} ?>
+		</div>
 </div>
